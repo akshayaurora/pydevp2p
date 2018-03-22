f71f77d26d541108a53c8461db6a88efc54ea3ad
Akshay Arora <akshayaurora@gmail.com>
2018-03-22 17:51:21 +0530

    Update app.py

--- devp2p/app.py
+++ devp2p/app.py
@@ -84,7 +84,7 @@ node:
     # stop on every unhandled exception!
     gevent.get_hub().SYSTEM_ERROR = BaseException  # (KeyboardInterrupt, SystemExit, SystemError)
 
-    print config
+    print(config)
     # create app
     app = BaseApp(config)
 
